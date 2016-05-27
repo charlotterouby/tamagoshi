c469f7782ff52b1a5b632768124fb5af4da50dbb
kohaistyle <kohaistyle@gmail.com>
2016-05-27 09:37:04 +0200

    Life factory

---
+++ assets/js/core/factories/life-factory.ts
@@ -0,0 +1,33 @@
+// PlayerFactory
+/// <reference path="../angular.d.ts"/>
+/// <reference path="player-factory.ts"/>
+
+module Application.Factories {
+
+	export class LifeFactory{
+
+
+		constructor (){
+			
+			console.log('Life Factory loaded');
+			//return this;
+
+		}
+
+	 	wash( goshi: any ): any{
+
+			 console.log( goshi.name + ' is washing');
+
+			//return player;
+		}
+
+		eat( goshi: any): any{
+
+			console.log(goshi.name + ' is eating');
+
+		}
+
+
+
+	}
+}
